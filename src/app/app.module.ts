a53f40bb2cdd1437990bb5cd4752b3e290ae2405
dhruv11111997 <dhruv.rawat278@gmail.com>
2021-03-08 16:13:27 +0530

    3 files modified, 14 added, 14 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ProductComponent } from './product/product.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { ProductComponent } from "./product/product.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HelloComponent, ProductComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,106 +1,132 @@
-import { Component, OnInit } from '@angular/core';
-import { fade } from './animations';
+import { Component, OnInit } from "@angular/core";
+import { fade } from "./animations";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css'],
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"],
   animations: fade
-
 })
 export class ProductComponent implements OnInit {
-
-  state = 'in';
+  state = "in";
   counter = 0;
   enableAnimation = false;
-  imageSource = '';
+  imageSource = "";
 
-
-  images: any[]=[
-    {img : '../assets/images/C_1.jpg'},
-    {img: '../assets/images/C_3.jpg'},
-
-    {img: '../assets/images/C_6.jpg'},
-     { img: '../assets/images/C_7.jpg'},
-      {img: '../assets/images/C_10.jpg'},
-      {img: '../assets/images/C_13.jpg'},
-      {img: '../assets/images/C_15.jpg'},
-      {img: '../assets/images/C_16.jpg'},
-      {img: '../assets/images/C_17.jpg'},
-      {img: '../assets/images/C_18.jpg'},
-      {img: '../assets/images/C_22.jpg'},
-      {img:'../assets/images/C_23.jpg'},
-      {img:'../assets/images/C_25.jpg'},
-      {img: '../assets/images/C_27.jpg'},
-      {img:'../assets/images/C_29.jpg'},
-      {img: '../assets/images/C_30.jpg'},
-      {img: '../assets/images/C_31.jpg'},
-
-      {img: '../assets/images/C_32.jpg'},
-      {img: '../assets/images/C_35.jpg'},
-
+  images: any[] = [
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/1.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/2.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/3.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/4.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/5.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/6.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/7.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/8.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/9.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/10.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/11.png"
+    },
+    {
+      img:
+        "https://stackblitz.com/files/angular-ivy-gpv3en/github/dhruv11111997/angular-ivy-gpv3en/master/src/app/assets/sample-product/12.png"
+    }
   ];
 
   ngOnInit() {
     this.imageSource = this.images[0].img;
-    this.initiate();
+    // this.initiate();
   }
-  drag = (data, event:any) => {
+change(){
+this.imageSource = this.images[0].img;
+console.log('----------imageSource---------------',this.imageSource);
+this.counter == this.counter + 1;
+if(this.images.length === this.counter){
+  this.counter = 0;
+}
+
+}
+
+
+  drag = (data, event: any) => {
     console.log(data);
-    console.log("event", event)
-    if(data==="start"){
-
-    }else{
-
+    console.log("event", event);
+    if (data === "start") {
+    } else {
     }
-  }
+  };
 
-  over(event : any){
+  over(event: any) {
     this.enableAnimation = false;
     event.preventDefault();
-    
   }
 
-  out(event: any){
+  out(event: any) {
     this.enableAnimation = true;
     this.initiate();
-
   }
-  initiate(){
-    if(this.counter>17){
+  initiate() {
+    if (this.counter > 13) {
       this.counter = 0;
     }
     this.enableAnimation = true;
-    if(this.enableAnimation){
+    if (this.enableAnimation) {
       this.toggggleSState();
     }
   }
 
-  toggleImges(){
-    this.imageSource = this.images[this.counter].img;
-   
+  toggleImges() {
+    this.imageSource = this.images[this.counter]?.img;
+
     this.initiate();
   }
 
-
   onDone($event) {
-      if(this.enableAnimation){
-      if(this.counter >0){
+    if (this.enableAnimation) {
+      if (this.counter > 0) {
         this.toggleImges();
-      }
-      else{
+      } else {
         this.toggggleSState();
       }
     }
   }
 
-  toggggleSState(){
-    if (this.counter%2 == 0) {
-      this.state = 'in';
+  toggggleSState() {
+    if (this.counter % 2 == 0) {
+      this.state = "in";
       this.counter++;
-
-    }
-    else{
+    } else {
       this.state = "out";
       this.counter++;
     }
@@ -113,44 +139,41 @@ export class ProductComponent implements OnInit {
   }
 
   onDrag(event: DragEvent) {
-    console.log('dragging', event);
+    console.log("dragging", event);
   }
-  val=0;
+  val = 0;
   onDragOver(event: DragEvent) {
-   console.log("----dhruvdddddddddd");
-    if(this.counter>17){
+    console.log("----dhruvdddddddddd");
+    if (this.counter > 17) {
       this.counter = 0;
     }
-    console.log('drag over', event);
-    if(event.x%9 == 0){
-      if(this.val < event.x){
-        this.counter = this.counter+1;
-      this.imageSource = this.images[this.counter].img;
-      this.val = event.x;
+    console.log("drag over", event);
+    if (event.x % 9 == 0) {
+      if (this.val < event.x) {
+        this.counter = this.counter + 1;
+        this.imageSource = this.images[this.counter]?.img;
+        this.val = event.x;
+      } else {
+        this.counter = this.counter - 1;
+        this.imageSource = this.images[this.counter]?.img;
+        this.val = event.x;
       }
-      else{
-        this.counter = this.counter-1;
-      this.imageSource = this.images[this.counter].img;
-      this.val = event.x;
-      }
-    } 
-   
+    }
   }
 
   onDragEnd(event: DragEvent) {
-    console.log('drag end', event);
+    console.log("drag end", event);
     // event.target.style.opacity = 1;
   }
   onDragLeave(event: DragEvent) {
-    console.log('drag leave', event);
+    console.log("drag leave", event);
   }
 
   onDrop(event: DragEvent) {
-    console.log('dropped', event);
+    console.log("dropped", event);
   }
 
   onDragEnter(event: DragEvent) {
-    console.log('drag enter', event);
-
+    console.log("drag enter", event);
   }
 }
